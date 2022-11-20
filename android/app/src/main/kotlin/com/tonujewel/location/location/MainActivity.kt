cc552998defd92b0674315b8c2fb4408f3b2d5dd
Jewel rana <tonujewel@gmail.com>
2022-11-20 12:27:09 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/tonujewel/location/location/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tonujewel.location.location
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+import 'second_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  var lat;
+  locationService() async {
+    Location location = Location();
+    LocationData? _locationData;
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    location.enableBackgroundMode(enable: true);
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    _locationData = await location.getLocation();
+
+    location.onLocationChanged.listen((LocationData currentLocation) {
+      // Use current location
+      setState(() {
+        print("lat ${currentLocation.latitude}");
+        lat = currentLocation.latitude;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    locationService();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("${lat}"),
+            TextButton(
+                onPressed: () {
+                  //
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const SecondPage()),
+                  );
+                },
+                child: Text("Next page"))
+          ],
+        ),
+      ),
+    );
+  }
+}
